576517b8f5b5aa9741f638794e2211e7f22f8194
Mesut Söğütlü <mesutsogutlu@192.168.25.109>
2026-01-15 16:47:13 +0300

    Remove global toaster overlay

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <Toaster />
+        {/* <Toaster /> Moved to Dashboard to prevent login blocking */}
       </body>
     </html>
   );
