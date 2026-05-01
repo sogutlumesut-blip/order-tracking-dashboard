2d3a0976c95b7c5b8928cb4e5a3e633abcc766b4
Mesut Söğütlü <mesutsogutlu@192.168.25.114>
2026-05-01 11:54:57 +0300

    Fix ServerActions payload size limit for Next 16

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
+    serverExternalPackages: [],
     experimental: {
         serverActions: {
             bodySizeLimit: '20mb',
